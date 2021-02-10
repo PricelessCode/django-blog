aff3ae3fa2de4ac53e74e1d2105ef7e086c8167d
Seungho Lee <poream3387@gmail.com>
2021-02-10 13:04:01 +0900

    Add base template and add Bootstrap

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig', # You need to add created apps for django to properly detect templates and databases later on.
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
