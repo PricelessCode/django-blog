6b5d46b8054973a0b151d89e5af7fb4f6f837b93
Seungho Lee <poream3387@gmail.com>
2021-02-12 19:09:25 +0900

    Add Post model to admin panel

--- django_blog/blog/admin.py
+++ django_blog/blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+# If you put the model here you will be able to see it on /admin site
+admin.site.register(Post)
