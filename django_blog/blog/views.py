1445992e0a044f2b2fbbc31f2fd8e20c7c026a4f
Seungho Lee <poream3387@gmail.com>
2021-02-20 23:43:07 +0900

    Add class based view for home ListView and post Detailview

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post # . means from this folder
 
-def home(request):
-    context = {
-        'posts': Post.objects.all()
-    }
-    return render(request, 'blog/home.html', context)
+# Function based view
+# def home(request):
+#     context = {  
+#         'posts': Post.objects.all()
+#     }
+#     return render(request, 'blog/home.html', context)
+
+# Class based View
+class PostListView(ListView):
+    model = Post # Which model to query
+    template_name = 'blog/home.html' # if you don't add this, the class will look for <app>/<model>_<viewtype>.html which I don't have
+    context_object_name = 'posts' # If you don't set this, you have to use name 'object' instead of 'posts' in the html template
+    ordering = ['-date_posted'] # -date_posted mean reversed ordering of date_posted so that newest post is on the bottom
+
+# Class based View
+class PostDetailView(DetailView):
+    model = Post
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
