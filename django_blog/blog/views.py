ac4cfd2fc6ef68367f6581fd88dd476abee18b00
Seungho Lee <poream3387@gmail.com>
2021-02-24 15:38:30 +0900

    Add PostCreateView and PostUpdateView

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post # . means from this folder
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # Function based view
 # def home(request):
@@ -19,6 +20,31 @@ class PostListView(ListView):
 # Class based View
 class PostDetailView(DetailView):
     model = Post
+
+# Add 'LoginRequiredMixin' for redirecting user if not logged in for this view
+class PostCreateView(LoginRequiredMixin, CreateView): 
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+# 'UserPassesTestMixin' for adding requirement
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, CreateView): 
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    # This is the required test for 'UserPassesTestMixin' (To prevent users from updating other users post)
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
