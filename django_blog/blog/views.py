7008e056ee279db4cd6dcc46e7e513bad6e3de86
Seungho Lee <poream3387@gmail.com>
2021-02-24 16:06:42 +0900

    Add PostDeleteView

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post # . means from this folder
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
@@ -26,6 +26,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
 
+    # This tells who is the author of this post
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
@@ -40,6 +41,17 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
         return super().form_valid(form)
 
     # This is the required test for 'UserPassesTestMixin' (To prevent users from updating other users post)
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/' # Go back to home if deleted successfully
+
+    # This is the required test for 'UserPassesTestMixin' (To prevent users from deleting other users post)
     def test_func(self):
         post = self.get_object()
         if self.request.user == post.author:
